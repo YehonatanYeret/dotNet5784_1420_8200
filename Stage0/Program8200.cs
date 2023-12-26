bceb9c7153d4f53e923f9f2f58ece0a6cdac1e08
Maor Noy <manoy@g.jct.ac.il>
2023-12-26 17:17:28 +0200

    create content from the welcome func

---
+++ Stage0/Program8200.cs
@@ -0,0 +1,11 @@
+﻿public class Class1
+{
+    public partial Class1()
+    {
+        public static void welcome8200()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+
+}
