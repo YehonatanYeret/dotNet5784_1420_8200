88affe682d0aa3faac9575928445f8c9c5ac0562
YehonatanYeret <yyeret@g.jct.ac.il>
2023-12-26 17:07:04 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program.1420.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Stage0
+{
+    partial class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            welcome1420();
+            welcome8200();
+            Console.ReadKey();
+        }
+
+        static partial void welcome8200();
+        private static void welcome1420()
+        {
+            Console.Write("Enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", username);
+        }
+    }
+}
