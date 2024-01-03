6ca18fccc13b6c2bbc32a7e3603473b565e21619
YehonatanYeret <yyeret@g.jct.ac.il>
2024-01-03 10:30:02 +0200

    propeties of Engineer and Mailestone

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -1,7 +1,13 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿namespace DO;
 
-namespace DO;
-
+/// <summary>
+/// Engineer entitiy for the database. represents an engineer in the project with all the relevant data
+/// </summary>
+/// <param name="ID">the ID of the Engineer</param>
+/// <param name="Cost">how much the Engineer take for hour</param>
+/// <param name="Name">the name of the Engineer</param>
+/// <param name="Email">the email of the Engineer</param>
+/// <param name="Level">the level of the Engineer - from Beginner to Expert</param>
 public record Engineer
 (
     int ID,
@@ -9,12 +15,7 @@ public record Engineer
     string Name,
     string Email,
     DO.EngineerExperience Level
-
- 
-
-
-     
 )
 { 
-    public Engineer() : this(0, "", "", 0) { }  
+    public Engineer() : this(0, 0, "", "", DO.EngineerExperience.Beginner) { }// empty ctor for Engineer
 }
--- DalFacade/DO/Milestone.cs
+++ DalFacade/DO/Milestone.cs
@@ -1,5 +1,19 @@
 ﻿namespace DO;
 
+
+/// <summary>
+/// Milestone entitiy for the database. represents a milestone in the project with all the relevant data
+/// </summary>
+/// <param name="ID">the ID of the Milestone</param>
+/// <param name="DependentTask">the ID of the Task that depend on this Mailestone</param>
+/// <param name="DependentOnTask">the ID of the last Task that our Task depend on </param>
 public record Milestone
-();
+(
+   int ID,
+   int? DependentTask = null,
+   int? DependentOnTask = null
+)
+{ 
+    public Milestone() : this(0) { }// empty ctor for Milestone
+}
 
