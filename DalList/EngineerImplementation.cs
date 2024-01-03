f80e3df1a7d4d7a8ccc1ff24e0fcbf19fb128982
YehonatanYeret <yyeret@g.jct.ac.il>
2024-01-03 15:11:12 +0200

    marge

--- DalList/EngineerImplementation.cs
+++ DalList/EngineerImplementation.cs
@@ -1,32 +1,51 @@
 ﻿namespace Dal;
 using DalApi;
 using DO;
-using System.Collections.Generic;
 
 internal class EngineerImplementation : IEngineer
 {
     public int Create(Engineer item)
     {
-        throw new NotImplementedException();
-    }
+        Engineer? engineer = DataSource.Engineers.Find(engineer => engineer.Id == item.Id);
+        if(engineer != null)//if the engineer already exists
+            throw new Exception($"Engineer with ID={item.Id} already exists");
 
-    public void Delete(int id)
-    {
-        throw new NotImplementedException();
+        DataSource.Engineers.Add(item);//add the task to the list
+        return item.Id;//return the id
     }
 
     public Engineer? Read(int id)
     {
-        throw new NotImplementedException();
+        Engineer? engineer = DataSource.Engineers.Find(engineer => engineer.Id == id);//find the engineer with the id and if not found return null
+        if (engineer == null)//if not found
+            throw new Exception($"Engineer with ID={id} does not exist");//throw exception
+        return engineer;
     }
 
     public List<Engineer> ReadAll()
     {
-        throw new NotImplementedException();
+        return new List<Engineer>(DataSource.Engineers);//return a copy of the list of engineers
     }
 
     public void Update(Engineer item)
     {
-        throw new NotImplementedException();
+        Engineer? engineer = DataSource.Engineers.Find(engineer => engineer.Id == item.Id);//find the index of the engineer with the same id
+        if (engineer == null)//if not found
+            throw new Exception($"Engineer with ID={item.Id} does not exist");//throw exception
+
+        DataSource.Engineers.Remove(engineer);//remove the engineer
+        DataSource.Engineers.Add(item);//add the new engineer
+    }
+    public void Delete(int id)
+    {
+        Task? task = DataSource.Tasks.Find(engineer => engineer.EngineerId == id);//find the index of the task with the same id that the engineer is working on
+        if (task != null)//the engineer is working on a task
+            throw new Exception($"cannot delete the engineer with the ID={id} because he works on the task with the ID={task.Id}");
+
+        Engineer? engineer = DataSource.Engineers.Find(engineer => engineer.Id == id);//find the index of the engineer with the same id
+        if (engineer == null)//if not found
+            throw new Exception($"Engineer with ID={id} does not exist");//throw exception
+
+        DataSource.Engineers.Remove(engineer);//remove the engineer
     }
 }
