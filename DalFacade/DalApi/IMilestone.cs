0015a46b678a00c7e4f68438c6322c6de50f72f1
Maor Noy <manoy@g.jct.ac.il>
2024-01-03 12:17:19 +0200

    setting files for the API

---
+++ DalFacade/DalApi/IMilestone.cs
@@ -0,0 +1,11 @@
+﻿namespace DalApi;
+using DO;
+
+public interface IMilestone
+{
+    int Create(Milestone item); //Creates new entity object in DAL
+    Milestone? Read(int id); //Reads entity object by its ID 
+    List<Milestone> ReadAll(); //stage 1 only, Reads all entity objects
+    void Update(Milestone item); //Updates entity object
+    void Delete(int id); //Deletes an object by its Id
+}
