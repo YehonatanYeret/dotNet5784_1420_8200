57d0f289df3e121ebfa330229a78a83331f4ffaf
YehonatanYeret <yyeret@g.jct.ac.il>
2024-01-23 17:34:57 +0200

    open the BO folders

---
+++ BL/BO/Engineer.cs
@@ -0,0 +1,11 @@
+﻿namespace BO;
+
+public class Engineer
+{
+    public int Id { get; init; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public EngineerExperience Level { get; set; }
+    public double cost { get; set; }
+    public TaskInEngineer Task { get; set; }
+}
---
+++ BL/BO/Enums.cs
@@ -0,0 +1,6 @@
+﻿namespace BO;
+
+/// <summary>
+/// The type of the engineer.
+/// </summary>
+public enum EngineerExperience { Beginner, AdvancedBeginner, Intermediate, Advanced, Expert }
