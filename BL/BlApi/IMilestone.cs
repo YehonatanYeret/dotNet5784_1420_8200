1c6a1840daab904f8ef6d571fa1f9430bddcf597
YehonatanYeret <yyeret@g.jct.ac.il>
2024-01-24 18:29:07 +0200

    creat the ITask and IMilestone files

--- BL/BlApi/IMilestone.cs
+++ BL/BlApi/IMilestone.cs
@@ -1,5 +1,11 @@
 ﻿namespace BlApi;
 
+/// <summary>
+/// Milestone interface
+/// </summary>
 public interface IMilestone
 {
+    //public void Create(BO.Milestone milestone);
+    public BO.Milestone Read(int id);
+    public BO.Milestone Rea(int id);
 }
--- BL/BlApi/ITask.cs
+++ BL/BlApi/ITask.cs
@@ -1,5 +1,13 @@
 ﻿namespace BlApi;
 
+/// <summary>
+///  Task interface
+/// </summary>
 public interface ITask
 {
+    public void Create(BO.Task task);
+    public BO.Task Read(int id);
+    public IEnumerable<BO.Task> ReadAll(Func<BO.Task, bool>? filter = null);
+    public void Update(BO.Task task);
+    public void Delete(int id);
 }
