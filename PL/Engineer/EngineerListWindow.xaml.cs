6fd4aff028b7a702f543733d2d2ec0916530c160
Maor Noy <manoy@g.jct.ac.il>
2024-02-12 23:12:02 +0200

    document

--- PL/Engineer/EngineerListWindow.xaml.cs
+++ PL/Engineer/EngineerListWindow.xaml.cs
@@ -11,10 +11,11 @@ namespace PL.Engineer
     /// </summary>
     public partial class EngineerListWindow : Window
     {
+        // Business logic layer instance
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
 
         /// <summary>
-        /// Dependency Property for list of Engineer
+        /// Dependency Property for list of Engineers
         /// </summary>
         public IEnumerable<BO.Engineer> EngineerList
         {
@@ -22,28 +23,46 @@ namespace PL.Engineer
             set { SetValue(EngineerListProperty, value); }
         }
 
+        /// <summary>
+        /// Dependency property for EngineerList
+        /// </summary>
         public static readonly DependencyProperty EngineerListProperty =
             DependencyProperty.Register("EngineerList", typeof(IEnumerable<BO.Engineer>), typeof(EngineerListWindow), new PropertyMetadata(null));
 
+        /// <summary>
+        /// Selected engineer's experience level
+        /// </summary>
         public BO.EngineerExperience experience { get; set; } = BO.EngineerExperience.None;
 
+        /// <summary>
+        /// Constructor for EngineerListWindow
+        /// </summary>
         public EngineerListWindow()
         {
             InitializeComponent();
             EngineerList = s_bl.Engineer.ReadAll()!;
         }
 
+        /// <summary>
+        /// Event handler for selection changed in the ComboBox
+        /// </summary>
         private void Expirience_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             UpdateListView();
         }
 
+        /// <summary>
+        /// Event handler for the "Add" button click
+        /// </summary>
         private void AddBtn_OnClick(object sender, RoutedEventArgs e)
         {
             new Engineer.EngineerWindow().ShowDialog();
             UpdateListView();
         }
 
+        /// <summary>
+        /// Event handler for double click on the ListView item
+        /// </summary>
         private void UpdateListView_DoubleClick(object sender, RoutedEventArgs e)
         {
             BO.Engineer? EngineerInList = (sender as ListView)?.SelectedItem as BO.Engineer;
@@ -51,6 +70,9 @@ namespace PL.Engineer
             UpdateListView();
         }
 
+        /// <summary>
+        /// Updates the ListView based on the selected experience level
+        /// </summary>
         void UpdateListView()
         {
             EngineerList = (experience == BO.EngineerExperience.None) ?
--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -1,23 +1,39 @@
-﻿namespace PL;
+﻿using System.Windows;
 
-using System.Windows;
-
+namespace PL;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window
 {
+    /// <summary>
+    /// Constructor for the MainWindow class.
+    /// Initializes the main window and its components.
+    /// </summary>
     public MainWindow()
     {
         InitializeComponent();
     }
 
+    /// <summary>
+    /// Event handler for the "Handle Engineers" button click event.
+    /// Opens the EngineerListWindow.
+    /// </summary>
+    /// <param name="sender">The object that raised the event.</param>
+    /// <param name="e">The event data.</param>
     private void EngineerList_click(object sender, RoutedEventArgs e)
     {
         new Engineer.EngineerListWindow().Show();
     }
 
+    /// <summary>
+    /// Event handler for the "Initialize DataBase" button click event.
+    /// Shows a message box asking the user if they want to initialize the data.
+    /// If the user confirms, initializes the data using DalTest.Initialization.Do().
+    /// </summary>
+    /// <param name="sender">The object that raised the event.</param>
+    /// <param name="e">The event data.</param>
     private void Initialization_click(object sender, RoutedEventArgs e)
     {
         if (MessageBoxResult.Yes == MessageBox.Show("Do you want to initialization the data?", "Initialization", MessageBoxButton.YesNo))
