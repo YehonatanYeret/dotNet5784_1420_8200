218a9d85b7671221beb74970d4b90f225d98a024
Maor Noy <manoy@g.jct.ac.il>
2024-02-13 17:54:25 +0200

    add engineer

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -17,28 +17,33 @@ public partial class MainWindow : Window
     }
 
     /// <summary>
-    /// Event handler for the "Handle Engineers" button click event.
-    /// Opens the EngineerListWindow.
-    /// </summary>
-    /// <param name="sender">The object that raised the event.</param>
-    /// <param name="e">The event data.</param>
-    private void EngineerList_click(object sender, RoutedEventArgs e)
-    {
-        new Engineer.EngineerListWindow().Show();
-    }
-
-    /// <summary>
     /// Event handler for the "Initialize DataBase" button click event.
     /// Shows a message box asking the user if they want to initialize the data.
     /// If the user confirms, initializes the data using DalTest.Initialization.Do().
     /// </summary>
     /// <param name="sender">The object that raised the event.</param>
     /// <param name="e">The event data.</param>
-    private void Initialization_click(object sender, RoutedEventArgs e)
+
+    //private void Initialization_click(object sender, RoutedEventArgs e)
+    //{
+    //    if (MessageBoxResult.Yes == MessageBox.Show("Do you want to initialization the data?", "Initialization", MessageBoxButton.YesNo))
+    //    {
+    //        DalTest.Initialization.Do();
+    //    }
+    //}
+
+    private void Button_Click(object sender, RoutedEventArgs e)
     {
-        if (MessageBoxResult.Yes == MessageBox.Show("Do you want to initialization the data?", "Initialization", MessageBoxButton.YesNo))
-        {
-            DalTest.Initialization.Do();
-        }
+
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        //new Manager.ManagerWindow().Show();
+    }
+
+    private void Button_Click_2(object sender, RoutedEventArgs e)
+    {
+        new Engineer.ShowEngineer().Show();
     }
 }
---
+++ PL/Messeges/IdInput.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PL.Messeges
+{
+    /// <summary>
+    /// Interaction logic for IdInput.xaml
+    /// </summary>
+    public partial class IdInput : UserControl
+    {
+        public IdInput()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+
+        public string Title { get; set; }
+
+        public int MaxLength { get; set; }
+
+    }
+}
